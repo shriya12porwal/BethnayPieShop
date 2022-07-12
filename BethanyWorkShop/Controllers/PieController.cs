4ca0724ee016fbbee7d9ccc7e0dd2fe712316b27
shriya.porwal <shriya.porwal@globallogic.com>
2022-07-12 17:35:46 +0530

    Add project files.

---
+++ BethanyWorkShop/Controllers/PieController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BethanyWorkShop.Models;
+
+namespace BethanyWorkShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository pieRepostior ;
+        public PieController(IPieRepository pieRepostior)
+        {
+            this.pieRepostior = pieRepostior;
+        }
+        public ViewResult List()
+        {
+            var pies = pieRepostior.AllPies;
+            return View(pies);
+        }
+    }
+}
